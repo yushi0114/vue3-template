939b01f123d32f2e1930c32b5c82df2aeecb1bf4
wangbinyu <binyu1231@gmail.com>
2022-11-08 11:09:46 +0800

    perf: css var

--- src/components/PagePanel.vue
+++ src/components/PagePanel.vue
@@ -10,39 +10,38 @@ withDefaults(
 const route = useRoute();
 
 onMounted(() => {
-    console.log('page permission', route.meta.permission);
+    route;
+    // console.log('page permission', route.meta.permission);
 });
 </script>
 
 <template>
-  <Layout class="page-panel" v-bind="$attrs">
-    <!-- -->
-    <header class="page-panel-header" v-if="title">
-        <Title>{{ title }}</Title>
-        <div class="page-panel-header__rest">
-            <button>delete</button>
-            <button>edit</button>
-            <button>edit</button>
-        </div>
-    </header>
-    <Layout class="page-panel-content">
-        <slot />
+    <Layout class="page-panel" v-bind="$attrs">
+        <!-- -->
+        <header class="page-panel-header" v-if="title">
+            <Title>{{ title }}</Title>
+            <div class="page-panel-header__rest">
+                <button>delete</button>
+                <button>edit</button>
+                <button>edit</button>
+            </div>
+        </header>
+        <Layout class="page-panel-content">
+            <slot />
+        </Layout>
     </Layout>
-  </Layout>
 </template>
 
 <style lang="postcss">
 .page-panel {
-  @apply flex-1 bg-green;
+    @apply flex-1;
 }
 
 .page-panel-header {
-  @apply flex mb-2;
+    @apply flex mb-2;
 }
 
 .page-panel-header__rest {
     @apply flex-1 flex justify-end;
 }
-
-
 </style>

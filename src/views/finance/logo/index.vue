a039093eb654809a6eab19030501674dbe94b467
wangbinyu <binyu1231@gmail.com>
2022-12-09 11:28:26 +0800

    perf: 机构LOGO 组件UI

--- src/views/finance/logo/index.vue
+++ src/views/finance/logo/index.vue
@@ -1,22 +1,12 @@
 <template>
     <PagePanel>
-        <Board full>
-            <FinancelogoContentList />
-        </Board>
+        <FinancelogoContentList />
     </PagePanel>
 </template>
 
 <script lang="ts" setup>
 
 import FinancelogoContentList from './components/finance-logo-content-list.vue';
-import { LoadingService } from '@/views/system/loading-service';
-import { getPageList } from './components/finance-logo';
-
-onMounted(async() => {
-    LoadingService.getInstance().loading();
-    await getPageList();
-    LoadingService.getInstance().stop();
-});
 </script>
 
 

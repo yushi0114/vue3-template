1d85b387e065152f148534c8ed62e9c20550855d
wangbinyu <binyu1231@gmail.com>
2022-12-28 15:30:45 +0800

    style: 调整报告详情代码结构

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,4 +8,4 @@ export { default as Empty } from './Empty.vue';
 export { default as SearchDropdown } from './SearchDropdown.vue';
 export * from './list';
 export * from './table';
-export * from './ScoreResult';
+export * from './scoreResult';

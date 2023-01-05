4dc62677315a12b934c796a6fc27562696257c5a
zhangyushi <zhangyushi@sjcredit.com>
2023-01-05 15:58:58 +0800

    🎈 perf: 新建编辑新闻政策去掉tabs

--- src/views/article/news/Create.vue
+++ src/views/article/news/Create.vue
@@ -3,15 +3,12 @@ import ArticlePublish from '../components/ArticlePublish.vue';
 import { ARTICLE_MODULE, PlatformType } from '@/enums';
 const route = useRoute();
 const platform = ref<PlatformType>(Number(route.params.type));
-function handleTabChange(plat: PlatformType) {
-    platform.value = plat;
-}
+
 </script>
 
 <template>
     <PagePanel>
         <Board class="article-create">
-            <PlatformTab @tab-change="handleTabChange"></PlatformTab>
             <article-publish v-if="platform === PlatformType.LiaoXinTong" :module="ARTICLE_MODULE.NEWS_LXT"></article-publish>
             <article-publish v-if="platform === PlatformType.ShiZongFu" :module="ARTICLE_MODULE.NEWS_ZJFW"></article-publish>
         </Board>
